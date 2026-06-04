6cf8b125686ac8b431849f9352999c1984fc58f3
Kyra Paraiso <24mashpotato@gmail.com>
2026-06-04 11:18:34 +0800

    changed something

--- straydex/desc/vi.py
+++ straydex/desc/vi.py
@@ -1,42 +1,33 @@
 class SD_VI_TIER_DESC:
-    one = """> **Clan Wars**
+    one = """- **Clan Wars**
 > <:8083:1511920091279921262> Icy Snow • [Server Link](https://discord.gg/cZ6C9twYTF)
-
-> **EP Paradise**
+- **EP Paradise**
 > <:8092:1511920078948667504> Continental • [Server Link](https://discord.gg/uxQvssN99q)
-
-> **Petal Solis**
+- **Petal Solis**
 > <:8089:1511920081431429261> Tundra • [Server Link](https://discord.gg/ApVGstgxk)"""
 
-    two = """> **VNA**
+    two = """- **VNA**
 > <:8104:1511920051073323048> Marine • [Server Link](https://discord.gg/sbZ2WrWSUr)
-
-> **Random Corp**
+- **Random Corp**
 > <:8098:1511920055053451365> Elegant • [Server Link](https://discord.gg/T2XRg3t2fY)
-
-> **BSL**
+- **BSL**
 > <:8101:1511920053237317753> Modern • [Server Link](https://discord.gg/bslpm)"""
 
-    three = """> **Bamboo Forest**
+    three = """- **Bamboo Forest**
 > <:8110:1511920046623162439> High Plains • [Server Link](https://discord.gg/bambooforest)
-
-> **Meowhelper**
+- **Meowhelper**
 > <:8116:1511920042210758766> River • [Server Link](https://discord.gg/MrAUGhtgY2)
-
-> **Scuffed Shop**
+- **Scuffed Shop**
 > <:8113:1511920044509102201> Sandstorm • [Server Link](https://discord.gg/8zHgfmAZdQ)
-
-> **Sanski**
+- **Sanski**
 > <:8122:1511920038297473094> Savanna • [Server Link](https://discord.gg/GaGFxHhSnS)"""
 
-    four = """> **Pokémon Legends ZA**
+    four = """- **Pokémon Legends ZA**
 > <:8128:1511920033931202641> Ocean • [Server Link](https://discord.gg/windsandwaves)
-
-> **Pokeden**
+- **Pokeden**
 > <:8125:1511920036414226525> Sun • [Server Link](https://discord.gg/TAEnYW4vV4)
-
-> **RPG Army**
+- **RPG Army**
 > <:8131:1511920030550458570> Jungle • [Server Link](https://discord.gg/67QE3uwRBw)"""
 
-    five = """> **Pokemeow Official Server**
+    five = """- **Pokemeow Official Server**
 > <:8134:1511920026926452786> Fancy • [Server Link](https://discord.gg/pokemeow)"""
